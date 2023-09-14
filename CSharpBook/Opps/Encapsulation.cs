bb138eae2435de532fe43685034aad90426e5b0c
Hemalatha Reddy <80377681+BHemalathareddy@users.noreply.github.com>
2023-09-14 14:58:05 +0530

    1st commit

---
+++ CSharpBook/Opps/Encapsulation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBook.Opps
+{
+    class BankAccount
+    {
+        private decimal balance;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance!");
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+    internal class Encapsulation
+    {
+        public void MainMethod()
+        {
+            // Using the BankAccount class
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Enter the amount how much you want to deposit");
+            string amount = Console.ReadLine();
+            decimal depositAmount = decimal.Parse(amount);
+            account.Deposit(depositAmount);
+            Console.WriteLine($"Deposited Amount = {depositAmount}");
+
+            Console.WriteLine("Enter the amount how much you want to withdraw");
+             amount = Console.ReadLine();
+            decimal withdrawAmount = decimal.Parse(amount);
+            account.Withdraw(withdrawAmount);
+            Console.WriteLine($"Withdraw Amount:{withdrawAmount}");
+
+            decimal balance = account.GetBalance();
+            Console.WriteLine("Current balance: " + balance);
+        }
+
+    }
+}
---
+++ CSharpBook/Opps/Inheritance.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBook.Opps
+{
+    // Base class
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("The vehicle starts.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("The vehicle stops.");
+        }
+    }
+
+    // Derived class
+    /// <summary>
+    /// The derived class Car inherits from the Vehicle class using the : Vehicle syntax.
+    /// It adds an additional method called Accelerate.
+    /// </summary>
+    class Car : Vehicle
+    {
+        public void Accelerate()
+        {
+            Console.WriteLine("The car accelerates.");
+        }
+        
+    }
+
+    // Derived class
+    class Motorcycle : Vehicle
+    {
+        public void TwoWheeler()
+        {
+            Console.WriteLine("The vehicle is a Twowheeler.");
+        }
+        
+    }
+    // Derived class
+    /// <summary>
+    /// cycle class inherit from the motorcycle class and motor class inherit from vehicle
+    /// so cycle class can access both the class data
+    /// </summary>
+    class Cycle : Motorcycle
+    {
+        public void Break()
+        {
+            Console.WriteLine("The cycle breaks.");
+        }       
+    }
+    public class Inheritance
+    {
+        public void InheritanceMethod()
+        {
+            Car car = new Car();
+            car.Brand = "Ford";
+            car.Color = "Red";
+
+            car.Start();
+            car.Accelerate();
+            car.Stop();
+
+            Motorcycle motorcycle = new Motorcycle();
+            motorcycle.Brand = "Honda";
+            motorcycle.Color = "Blue";
+
+            motorcycle.Start();
+            motorcycle.TwoWheeler();
+            motorcycle.Stop();
+
+            Cycle cycle = new Cycle();
+            cycle.Brand = "Honda";
+            cycle.Color = "Black";
+
+            cycle.TwoWheeler();
+            cycle.Start();
+            cycle.Break();
+            
+        }
+    }
+}
